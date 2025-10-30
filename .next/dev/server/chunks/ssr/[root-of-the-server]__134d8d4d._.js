a30f035ab6e85a467ddef17ec50f36f8a0ffc82a
Bharat vala <bharatkumarramashibhai2003@gmail.com>
2025-10-30 12:15:43 +0530

    create stockdio

--- .next/dev/server/chunks/ssr/[root-of-the-server]__134d8d4d._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__134d8d4d._.js
@@ -7,7 +7,7 @@ module.exports = mod;
 }),
 "[project]/src/app/assets/logo.png (static in ecmascript, tag client)", ((__turbopack_context__) => {
 
-__turbopack_context__.v("./_next/static/media/logo.b69eee79.png");}),
+__turbopack_context__.v("/rupieTimes/_next/static/media/logo.b69eee79.png");}),
 "[project]/src/app/assets/logo.png.mjs { IMAGE => \"[project]/src/app/assets/logo.png (static in ecmascript, tag client)\" } [app-ssr] (structured image object with data url, ecmascript)", ((__turbopack_context__) => {
 "use strict";
 
--- .next/dev/static/development/_buildManifest.js
+++ .next/dev/static/development/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/.//_next/:path+",
-        "destination": "/_next/:path+"
+        "source": "/rupieTimes//_next/:path+",
+        "destination": "/rupieTimes/_next/:path+"
       }
     ],
     "fallback": []
